bb61a358b4c9f87390b4691a9b2b2d1fc2e51328
BlaBlaPrincess <simakin.alexander@mail.ru>
2021-08-17 17:44:58 +1000

    Initial commit

---
+++ src/main/java/com/github/blablaprincess/simpleexh/SimpleExhAutoConfiguration.java
@@ -0,0 +1,9 @@
+package com.github.blablaprincess.simpleexh;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConditionalOnWebApplication
+public class SimpleExhAutoConfiguration {
+}
