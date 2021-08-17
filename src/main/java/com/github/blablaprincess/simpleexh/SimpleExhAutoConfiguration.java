2307499500eff28564421b07fd83d1edccf8b207
BlaBlaPrincess <simakin.alexander@mail.ru>
2021-08-17 21:37:06 +1000

    Complete SimpleExhAutoConfiguration

--- src/main/java/com/github/blablaprincess/simpleexh/SimpleExhAutoConfiguration.java
+++ src/main/java/com/github/blablaprincess/simpleexh/SimpleExhAutoConfiguration.java
@@ -1,9 +1,29 @@
 package com.github.blablaprincess.simpleexh;
 
+import com.github.blablaprincess.simpleexh.errordtobuilder.DefaultErrorDtoBuilder;
+import com.github.blablaprincess.simpleexh.errordtobuilder.ErrorDtoBuilder;
+import com.github.blablaprincess.simpleexh.exceptioncoderesolver.DefaultExceptionCodeResolver;
+import com.github.blablaprincess.simpleexh.exceptioncoderesolver.ExceptionCodeResolver;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@ComponentScan("com.github.blablaprincess.simpleexh")
 @ConditionalOnWebApplication
 public class SimpleExhAutoConfiguration {
+
+    @Bean
+    @ConditionalOnMissingBean
+    ExceptionCodeResolver defaultExceptionCodeResolver() {
+        return new DefaultExceptionCodeResolver();
+    }
+
+    @Bean
+    @ConditionalOnMissingBean
+    ErrorDtoBuilder defaultErrorDtoBuilder() {
+        return new DefaultErrorDtoBuilder();
+    }
 }
